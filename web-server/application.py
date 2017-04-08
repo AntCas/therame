7e3353e67a56242c12e6b3945d1ac443d83dd822
Anthony Castrio <anthonycastrio@gmail.com>
2017-04-08 17:48:45 -0400

    adding graph skeleton

--- web-server/application.py
+++ web-server/application.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template(
-        'index.html', 
+        'index.html' 
     )
 
 # must set debug=False before public hosting
