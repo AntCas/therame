aeae1fe4fa610778ee43a6fbed04c8fb84a56884
Anthony Castrio <anthonycastrio@gmail.com>
2017-04-08 15:12:22 -0400

    flask app skeleton

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import (Flask, render_template, request, 
+                   make_response, url_for, redirect)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html', 
+    )
+
+# must set debug=False before public hosting
+app.run(debug=True, host='0.0.0.0', port=8000)
+
