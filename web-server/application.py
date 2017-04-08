c6401bbf2436d6d817c4c4695d84e508057e84fc
Anthony Castrio <anthonycastrio@gmail.com>
2017-04-08 18:37:18 -0400

    displaying a chart of one months step data

--- web-server/application.py
+++ web-server/application.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import (Flask, render_template, request, 
                    make_response, url_for, redirect)
 
@@ -5,8 +7,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # load json data
+    with open("./data/steps.json", 'r') as j:
+        step_data = json.load(j)
+
     return render_template(
-        'index.html' 
+        'index.html',
+        step_data = step_data["activities-steps"]
     )
 
 # must set debug=False before public hosting
