29292283c358491628a2b355b60fe287531191c2
Anthony Castrio <anthonycastrio@gmail.com>
2017-04-09 08:19:00 -0400

    getting data from DynamoDB

--- web-server/application.py
+++ web-server/application.py
@@ -1,10 +1,40 @@
 import json
 
+# DynamoDB
+import boto3
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
+
 from flask import (Flask, render_template, request, 
                    make_response, url_for, redirect)
 
+
 app = Flask(__name__)
 application = app
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
+
+def get_emotions():
+    table = dynamodb.Table('therapme-userData')
+
+    try:
+        response = table.scan()
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        return response
 
 @app.route('/')
 def index():
@@ -14,7 +44,8 @@ def index():
 
     return render_template(
         'index.html',
-        step_data = step_data["activities-steps"]
+        step_data = step_data["activities-steps"],
+        emotions_data = get_emotions()
     )
 
 if __name__ == '__main__':
